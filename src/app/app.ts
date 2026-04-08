36aba7f98ea375adc0135273bb01f952a231ece9
kenher-droid <huamanfrankkevin@gmail.com>
2026-04-08 18:37:36 -0500

    background añadido

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { HeroBanner } from './components/hero-banner/hero-banner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, HeroBanner],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
