43c7788fc5888d81eb08a761ddbb30a98ac53698
JosepAnderson <josepramirez83@gmail.com>
2026-04-15 20:04:42 -0500

    padding,nosotros,visitanos

--- src/app/components/main/nosotros/nosotros.ts
+++ src/app/components/main/nosotros/nosotros.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nosotros',
+  standalone: true, // Importante en Angular moderno
   imports: [],
   templateUrl: './nosotros.html',
   styleUrl: './nosotros.css',
