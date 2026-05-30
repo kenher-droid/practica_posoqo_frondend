1c005df5a5a4a03cbe0e90fa5cad9627b25d4c6c
kenher-droid <huamanfrankkevin@gmail.com>
2026-05-29 22:48:38 -0500

    arreglos de diseño (aun falta) x2

--- src/app/features/admin/layout/admin-layout/admin-layout.ts
+++ src/app/features/admin/layout/admin-layout/admin-layout.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterModule } from "@angular/router";
+import { RouterModule, Router } from "@angular/router";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './admin-layout.html',
   styleUrl: './admin-layout.css',
 })
-export class AdminLayout {}
+export class AdminLayout {
+  constructor(private router: Router) {}
+
+  logout(): void {
+    // TODO: Limpiar sesión y tokens
+    console.log('Cerrando sesión...');
+    this.router.navigate(['/auth/login']);
+  }
+}
