d92c14e7d4d56cf9118cd61c6d98b8d22ea111d3
JosepAnderson <josepramirez83@gmail.com>
2026-06-04 01:59:11 -0500

    josep

--- src/app/features/public/shared/sections/platos/platos.ts
+++ src/app/features/public/shared/sections/platos/platos.ts
@@ -16,11 +16,11 @@ interface Plato {
 export class Platos {
   // Array de datos con las rutas tal como lo solicitaste
   listaPlatos: Plato[] = [
-    { id: 1, nombre: 'arroz con pollo', imagen: 'assets/extras/alitas.png' },
-    { id: 2, nombre: 'arroz con pollo', imagen: 'assets/extras/alitas.png' },
-    { id: 3, nombre: 'arroz con pollo', imagen: 'assets/extras/alitas.png' },
-    { id: 4, nombre: 'arroz con pollo', imagen: 'assets/extras/alitas.png' },
-    { id: 5, nombre: 'arroz con pollo', imagen: 'assets/extras/alitas.png' },
-    { id: 6, nombre: 'arroz con pollo', imagen: 'assets/extras/alitas.png' }
+    { id: 1, nombre: 'arroz con pollo', imagen: 'assets/extras/alitas.webp' },
+    { id: 2, nombre: 'arroz con pollo', imagen: 'assets/extras/alitas.webp' },
+    { id: 3, nombre: 'arroz con pollo', imagen: 'assets/extras/alitas.webp' },
+    { id: 4, nombre: 'arroz con pollo', imagen: 'assets/extras/alitas.webp' },
+    { id: 5, nombre: 'arroz con pollo', imagen: 'assets/extras/alitas.webp' },
+    { id: 6, nombre: 'arroz con pollo', imagen: 'assets/extras/alitas.webp' }
   ];
 }
